336e5eb781e240e7d779a5a628988b5a5909d82d
Bryan Tamada <btamada@users.noreply.github.com>
2016-02-28 00:32:20 -0800

    Removed unrelated file

--- DeleteNodeLinkedList.java
+++
@@ -1,9 +0,0 @@
-public class DeleteNodeLinkedList {
-
-    public void deleteNode(ListNode node) {
-        ListNode temp = node.next;
-        node.val = temp.val;
-        node.next = temp.next;
-    }
-
-}
